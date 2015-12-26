52a54830e3842fed33bc4a1b4d91670f6385fd20
kushal tirumala <kushaltirumala99@gmail.com>
2015-12-25 17:20:55 -0800

    init

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+
+app.get('/', function(req, res) {
+	res.send('Hello World!');
+});
+
+app.get('/user', function(req, res) {
+	res.send('you cannot have user information?');
+});
+
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Listening on port: ', port);
+})

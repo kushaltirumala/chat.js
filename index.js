1538cf5e6759dcf07ed2ad7ebf5c9f3a943e6f20
kushal tirumala <kushaltirumala99@gmail.com>
2015-12-26 02:17:47 -0800

    in

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('a user connected!');
+	socket.on('chat message', function(msg) {
+		console.log('message: ' + msg)
+	})
 });
 
 app.get('/user', function(req, res) {
 	res.send('you cannot have user information?');
 });
 
-var server = app.listen(3000, function() {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Listening on port: ', port);
+http.listen(3000, function() {
+	console.log('Listening on port: 3000');
 })
